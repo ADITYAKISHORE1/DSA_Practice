0b6d10ed111a81f76d8b3f7d57e24762efe71ded
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-04-14 01:48:56 +0530

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int near=0,far=0,jmp=0;
+        while(far<nums.size()-1){
+            int farthest=0;
+            while(near<=far){
+                farthest=max(farthest,near+nums[near]);
+                near++;
+            }
+            far=farthest;
+            jmp++;
+        }
+        return jmp;
+    }
+};
