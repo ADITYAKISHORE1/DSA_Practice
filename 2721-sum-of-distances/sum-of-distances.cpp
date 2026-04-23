2edc18ba7c4c25da69d3a6a3d744b68178bec4e7
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-04-23 13:49:14 +0530

    Time: 51 ms (90.25%) | Memory: 105.8 MB (98.00%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.cpp
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -3,20 +3,19 @@ public:
     vector<long long> distance(vector<int>& nums) {
         int n = nums.size();
         vector<long long> arr(n, 0);
-        vector<pair<long long, long long>> left(n);
         unordered_map<long long, pair<long long, long long>> mpp;
         for(int i=0;i<n;i++){
-            mpp[nums[i]].first+=1;
-            mpp[nums[i]].second+=i;
-            left[i]={mpp[nums[i]].first,mpp[nums[i]].second};
+            auto& it=mpp[nums[i]];
+            it.first+=1;
+            it.second+=i;
+            arr[i]=(it.first*i)-it.second;
         }
         mpp.clear();
         for(int i=n-1;i>=0;i--){
-            mpp[nums[i]].first+=1;
-            mpp[nums[i]].second+=i;
-            long long l=(left[i].first*i)-left[i].second;
-            long long r=mpp[nums[i]].second-(mpp[nums[i]].first*i);
-            arr[i]=l+r;
+            auto& it=mpp[nums[i]];
+            it.first+=1;
+            it.second+=i;
+            arr[i]+=it.second-(it.first*i);
         }
         return arr;
     }
