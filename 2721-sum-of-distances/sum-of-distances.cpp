0e41072b09f8d03fff8180974916d32f17962185
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-04-23 13:52:00 +0530

    Time: 63 ms (76.25%) | Memory: 105.8 MB (97.00%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.cpp
+++ 2721-sum-of-distances/sum-of-distances.cpp
@@ -6,16 +6,14 @@ public:
         unordered_map<long long, pair<long long, long long>> mpp;
         for(int i=0;i<n;i++){
             auto& it=mpp[nums[i]];
-            it.first+=1;
             it.second+=i;
-            arr[i]=(it.first*i)-it.second;
+            arr[i]=(++it.first*i)-it.second;
         }
         mpp.clear();
         for(int i=n-1;i>=0;i--){
             auto& it=mpp[nums[i]];
-            it.first+=1;
             it.second+=i;
-            arr[i]+=it.second-(it.first*i);
+            arr[i]+=it.second-(++it.first*i);
         }
         return arr;
     }
