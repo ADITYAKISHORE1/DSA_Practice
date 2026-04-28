3df767366c4f9001e680a7ade56643fab2f0e593
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-04-28 20:15:59 +0530

    Time: 46 ms (37.45%) | Memory: 90.1 MB (53.91%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -6,30 +6,18 @@ public:
             for (auto& j : i)
                 v.push_back(j);
         sort(v.begin(), v.end());
-        int n = v.size();
-        int l = max(0, (n / 2) - 1), r = (n / 2);
-        int ans = INT_MAX;
-        int az = 0;
-        for (auto i : v) {
-            int a = abs(i - v[l]);
-            if (a % x != 0) {
-                az = 1e9;
-                break;
-            }
-            az += a/x;
-        }
+        int r = v[(v.size() / 2)];
         int ay = 0;
-        for (auto i : v) {
-            int a = abs(i - v[r]);
+        for (auto& i : v) {
+            int a = abs(i - r);
             if (a % x != 0) {
                 ay = 1e9;
                 break;
             }
-            ay += a/x;
+            ay += a / x;
         }
-        ans = min(az, ay);
-        if (ans == 1e9)
+        if (ay == 1e9)
             return -1;
-        return ans;
+        return (ay==1e9?-1:ay);
     }
 };
