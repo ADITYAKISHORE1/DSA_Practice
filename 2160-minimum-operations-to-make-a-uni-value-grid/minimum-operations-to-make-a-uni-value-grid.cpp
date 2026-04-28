b4629fc4af00d82a94307e975faea3e0424669fc
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-04-28 20:31:45 +0530

    Time: 28 ms (76.54%) | Memory: 90.2 MB (34.57%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -5,19 +5,27 @@ public:
         for (auto& i : grid)
             for (auto& j : i)
                 v.push_back(j);
-        sort(v.begin(), v.end());
-        int r = v[(v.size() / 2)];
-        int ay = 0;
-        for (auto& i : v) {
-            int a = abs(i - r);
-            if (a % x != 0) {
-                ay = 1e9;
-                break;
-            }
-            ay += a / x;
+        int rem=v[0]%x;
+        for(auto& i:v){
+            if(i%x!=rem) return -1;
+            i=i/x;
         }
-        if (ay == 1e9)
-            return -1;
-        return (ay==1e9?-1:ay);
+        auto cost =[&](int target){
+            int a=0;
+            for(auto& i:v){
+                a+=abs(i-target);
+            }
+            return a;
+        };
+        int l=*min_element(v.begin(),v.end());
+        int r=*max_element(v.begin(),v.end());
+        while(l<r){
+            int mid=l+(r-l)/2;
+            int c1=cost(mid);
+            int c2=cost(mid+1);
+            if(c1<=c2) r=mid;
+            else l=mid+1;
+        }
+        return cost(l);
     }
 };
