8dcf4fabfcd5debaf9e10f069bc0fc5aaa51a6da
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-05-31 11:29:16 +0530

    Time: 12 ms (8.75%) | Memory: 12.4 MB (8.99%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    string add(string& a, string& b) {
+        string ans;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int c = 0;
+
+        while (i >= 0 || j >= 0 || c) {
+            int s = c;
+
+            if (i >= 0)
+                s += a[i--] - '0';
+            if (j >= 0)
+                s += b[j--] - '0';
+
+            ans.push_back((s % 10) + '0');
+            c = s / 10;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+        string ans="0";
+        for (int i = num1.size() - 1; i >= 0; i--) {
+            string s;
+            int carry = 0;
+            for (int k = 0; k < num1.size() - i - 1; k++)
+                s += "0";
+            for (int j = num2.size() - 1; j >= 0; j--) {
+                int d1 = num1[i] - '0';
+                int d2 = num2[j] - '0';
+                int mul = d1 * d2 + carry;
+                s += ((mul % 10) + '0');
+                carry = mul / 10;
+            }
+            if (carry > 0)
+                s += (carry + '0');
+            reverse(s.begin(), s.end());
+            ans = add(ans, s);
+        }
+        return ans;
+    }
+};
