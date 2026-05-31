f9ca7013c70a2667b15e754512d2fa7800bd6081
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-05-31 13:50:14 +0530

    Time: 37 ms (42.91%) | Memory: 143.8 MB (52.85%) - LeetSync

--- 3031-construct-product-matrix/construct-product-matrix.cpp
+++ 3031-construct-product-matrix/construct-product-matrix.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        const int MOD=12345;
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<int> v;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                v.push_back(grid[i][j]);
-            }
+        const int MOD = 12345;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> a(n, vector<int>(m));
+        int pref = 1;
+        for (int i = 0; i < n * m; i++) { // pref calc
+            a[i / m][i % m] = pref;
+            pref = ((grid[i / m][i % m] % MOD) * pref) % MOD;
         }
-        int nm=v.size();
-        vector<int> a(nm,1);
-        for(int i=1;i<nm;i++){
-            a[i]=((a[i-1]%MOD)*(v[i-1]%MOD))%MOD;
+        int suf = 1;
+        for (int i = n * m - 1; i >= 0; i--) { // suf calc and final ans prep
+            a[i / m][i % m] = (a[i / m][i % m] * suf) % MOD;  // pref * suf
+            suf = (suf * (grid[i / m][i % m] % MOD)) % MOD;
         }
-        int suf=1;
-        for(int i=nm-2;i>=0;i--){
-            suf=((suf%MOD)*(v[i+1]%MOD))%MOD;
-            a[i]=((suf%MOD)*(a[i]%MOD))%MOD;
-        }
-        for(int i=0;i<nm;i++){
-            int x=(i/m);
-            int y=(i%m);
-            grid[x][y]=a[i];
-        }
-        return grid;
+        return a;
     }
 };
