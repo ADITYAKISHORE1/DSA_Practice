36233572b514236bda20b7cf35f1bc75c045b692
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-03 16:17:18 +0530

    Time: 151 ms (11.93%) | Memory: 270.8 MB (11.01%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -1,84 +1,56 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime,
-                           vector<int>& landDuration,
-                           vector<int>& waterStartTime,
-                           vector<int>& waterDuration) {
-
-        int n = landStartTime.size();
-        int m = waterStartTime.size();
-
-        vector<pair<int, int>> land, water;
-
-        for (int i = 0; i < n; i++)
-            land.push_back({landStartTime[i], landDuration[i]});
-
-        for (int i = 0; i < m; i++)
-            water.push_back({waterStartTime[i], waterDuration[i]});
-
-        sort(land.begin(), land.end());
-        sort(water.begin(), water.end());
-
-        vector<int> landStarts(n), waterStarts(m);
-
-        vector<int> prefLand(n), prefWater(m);
-        vector<int> sufLand(n), sufWater(m);
-
-        for (int i = 0; i < n; i++)
-            landStarts[i] = land[i].first;
-
-        for (int i = 0; i < m; i++)
-            waterStarts[i] = water[i].first;
-
-        prefLand[0] = land[0].second;
-        for (int i = 1; i < n; i++)
-            prefLand[i] = min(prefLand[i - 1], land[i].second);
-
-        prefWater[0] = water[0].second;
-        for (int i = 1; i < m; i++)
-            prefWater[i] = min(prefWater[i - 1], water[i].second);
-
-        sufLand[n - 1] = land[n - 1].first + land[n - 1].second;
-        for (int i = n - 2; i >= 0; i--)
-            sufLand[i] = min(sufLand[i + 1], land[i].first + land[i].second);
-
-        sufWater[m - 1] = water[m - 1].first + water[m - 1].second;
-        for (int i = m - 2; i >= 0; i--)
-            sufWater[i] =
-                min(sufWater[i + 1], water[i].first + water[i].second);
-
-        int ans = INT_MAX;
-
-        // Land -> Water
-        for (int i = 0; i < n; i++) {
-            int finishLand = landStartTime[i] + landDuration[i];
-
-            int idx = upper_bound(waterStarts.begin(), waterStarts.end(),
-                                  finishLand) -
-                      waterStarts.begin();
-
-            if (idx > 0)
-                ans = min(ans, finishLand + prefWater[idx - 1]);
-
-            if (idx < m)
-                ans = min(ans, sufWater[idx]);
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int m=landStartTime.size();
+        int n=waterStartTime.size();
+        vector<pair<int,int>> land,water;
+        for(int i=0;i<m;i++){
+            land.push_back({landStartTime[i],landDuration[i]});
         }
-
-        // Water -> Land
-        for (int i = 0; i < m; i++) {
-            int finishWater = waterStartTime[i] + waterDuration[i];
-
-            int idx =
-                upper_bound(landStarts.begin(), landStarts.end(), finishWater) -
-                landStarts.begin();
-
-            if (idx > 0)
-                ans = min(ans, finishWater + prefLand[idx - 1]);
-
-            if (idx < n)
-                ans = min(ans, sufLand[idx]);
+        for(int i=0;i<n;i++){
+            water.push_back({waterStartTime[i],waterDuration[i]});
         }
-
+        sort(land.begin(),land.end());
+        sort(water.begin(),water.end());
+        vector<int> prefLand(m),prefWater(n);
+        vector<int> sufLand(m),sufWater(n);
+        prefLand[0]=land[0].second;
+        for(int i=1;i<m;i++){
+            prefLand[i]=min(prefLand[i-1],land[i].second);
+        }
+        prefWater[0]=water[0].second;
+        for(int i=1;i<n;i++){
+            prefWater[i]=min(prefWater[i-1],water[i].second);
+        }
+        sufLand[m-1]=land[m-1].first+land[m-1].second;
+        for(int i=m-2;i>=0;i--){
+            sufLand[i]=min(sufLand[i+1],land[i].first+land[i].second);
+        }
+        sufWater[n-1]=water[n-1].first+water[n-1].second;
+        for(int i=n-2;i>=0;i--){
+            sufWater[i]=min(sufWater[i+1],water[i].first+water[i].second);
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<m;i++){
+            int landEnd=land[i].first+land[i].second;
+            int ub=upper_bound(water.begin(),water.end(),make_pair(landEnd,-1))-water.begin();
+            if(ub>0){
+                ans=min(ans,landEnd+prefWater[ub-1]);
+            }
+            if(ub<n){
+                ans=min(ans,sufWater[ub]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            int waterEnd=water[i].first+water[i].second;
+            int ub=upper_bound(land.begin(),land.end(),make_pair(waterEnd,-1))-land.begin();
+            if(ub>0){
+                ans=min(ans,waterEnd+prefLand[ub-1]);
+            }
+            if(ub<m){
+                ans=min(ans,sufLand[ub]);
+            }
+        }
         return ans;
     }
 };
