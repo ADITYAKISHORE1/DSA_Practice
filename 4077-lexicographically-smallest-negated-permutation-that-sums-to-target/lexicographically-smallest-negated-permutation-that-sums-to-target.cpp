56e7aa7c70cad0287aec15d359d60e48c50cd33c
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-05 12:39:36 +0530

    Time: 119 ms (45.50%) | Memory: 107.6 MB (18.96%) - LeetSync

--- 4077-lexicographically-smallest-negated-permutation-that-sums-to-target/lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
+++ 4077-lexicographically-smallest-negated-permutation-that-sums-to-target/lexicographically-smallest-negated-permutation-that-sums-to-target.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> lexSmallestNegatedPerm(int n, long long target) {
         vector<int> perm;
         long long sum=0;
-        for(int i=n;i>=1;i--){
+        for(int i=1;i<=n;i++){
             sum+=i;
             perm.push_back(i);
         }
         if(target>sum or (sum-target)&1) return {};
         long long diff=(sum-target)/2;
-        for(int i=0;i<n;i++){
+        for(int i=n-1;i>=0;i--){
             if(diff>=perm[i]){
                 diff-=perm[i];
                 perm[i]=-perm[i];
