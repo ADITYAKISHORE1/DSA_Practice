1b8bbd6aeef2a7643f20529d19576d7c963607a0
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-07 16:16:25 +0530

    Time: 83 ms (18.66%) | Memory: 58 MB (7.21%) - LeetSync

--- 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.cpp
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.cpp
@@ -1,27 +1,29 @@
 class Solution {
-    vector<string> f(int prev,int n,int k){
-        if(k<0 or n<0) return {};
-        if(n==0){
-            if(prev) return {"0"};
-            else return {"1","0"};
+    vector<string> f(int prev, int n, int k) {
+        if (k < 0)
+            return {};
+        if (n == 0) {
+            if (prev)
+                return {"0"};
+            else
+                return {"1", "0"};
         }
-            vector<string> st=f(0,n-1,k);
-        
-            for(auto& s:st){
-                s=s+"0";
-            }
-        if(!prev){
-            vector<string> st1=f(1,n-1,k-n);
-            for(auto &s:st1){
-                s=s+"1";
-                st.push_back(s);
+        vector<string> st = f(0, n - 1, k);
+
+        for (auto& s : st) {
+            s = s + "0";
+        }
+        if (!prev) {
+            vector<string> st1 = f(1, n - 1, k - n);
+            for (auto& s : st1) {
+                st.push_back(s+"1");
             }
         }
         return st;
     }
+
 public:
     vector<string> generateValidStrings(int n, int k) {
-        vector<string> st1=f(0,n-1,k);
-        return st1;
+        return f(0, n - 1, k);
     }
 };
