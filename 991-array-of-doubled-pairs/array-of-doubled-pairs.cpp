97c94720e41586c5bd5f92b19415241f9e3663f9
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-07 19:09:09 +0530

    Time: 137 ms (6.19%) | Memory: 101.7 MB (5.31%) - LeetSync

--- 991-array-of-doubled-pairs/array-of-doubled-pairs.cpp
+++ 991-array-of-doubled-pairs/array-of-doubled-pairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canReorderDoubled(vector<int>& arr) {
         multiset<int> st,st2;
-        for (auto i : arr) {
+        for (auto& i : arr) {
             if (i >= 0)
                 st.insert(i);
             else st2.insert(i);
@@ -10,9 +10,8 @@ public:
         while (!st.empty()) {
             int a = *st.begin();
             st.erase(st.begin());
-            auto lb = st.lower_bound(2 * a);
-            // cout << *lb << " " << 2 * a << endl;
             if(st.empty()) return false;
+            auto lb = st.lower_bound(2 * a);
             if (*lb != 2 * a)
                 return false;
             st.erase(lb);
@@ -21,9 +20,8 @@ public:
         while (!st2.empty()) {
             int a = *prev(st2.end());
             st2.erase(prev(st2.end()));
-            auto lb = st2.lower_bound(2 * a);
-            // cout << *lb << " " << 2 * a << endl;
             if(st2.empty()) return false;
+            auto lb = st2.lower_bound(2 * a);
             if (*lb != 2 * a)
                 return false;
             st2.erase(lb);
