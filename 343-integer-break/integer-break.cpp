bc5010804bc148f1e6dd04b6ce9f0f9d8a73b30d
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-09 01:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (30.92%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,19 +1,14 @@
 class Solution {
-    vector<vector<int>> dp;
-    int f(int n,int k){
-        if(n==0){
-            if(k>=2) return 1;
-            return 0;
-        }
-        if(dp[n][k>=2]!=-1) return dp[n][k>=2];
-        for(int i=1;i<=n;i++){
-            dp[n][k>=2]=max(dp[n][k>=2],i*f(n-i,k+1));
-        }
-        return dp[n][k>=2];
-    }
+    
+    
 public:
     int integerBreak(int n) {
-        dp.resize(n+1,vector<int>(2,-1));
-        return f(n,0);
+        vector<int> dp(n+1,0);
+        for(int i=2;i<=n;i++){
+                for(int k=1;k<i;k++){
+                    dp[i]=max({dp[i],k*dp[i-k],k*(i-k)});
+                }
+        }
+        return dp[n];
     }
 };
