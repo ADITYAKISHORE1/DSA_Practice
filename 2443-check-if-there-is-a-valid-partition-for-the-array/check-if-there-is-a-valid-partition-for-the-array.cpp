47713e4a85fdcb462c58b85f7ff593bc6bca7653
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-09 02:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 91.7 MB (51.98%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,46 +1,21 @@
 class Solution {
-    bool f(int i, vector<int>& v, vector<int>& dp) {
-        if (i < 0)
-            return true;
-        if (i == 0)
-            return false;
-        if (i == 1) {
-            return v[0] == v[1];
-        }
-        if (dp[i] != -1)
-            return dp[i];
-        bool take = 0;
-        if (v[i - 1] == v[i]) {
-            take = 1 & f(i - 2, v, dp);
-        }
-        if (v[i - 2] == v[i - 1] and v[i - 1] == v[i]) {
-            take |= (1 & f(i - 3, v, dp));
-        }
-        if (v[i - 2] == v[i - 1] - 1 and v[i - 1] == v[i] - 1) {
-            take |= (1 & f(i - 3, v, dp));
-        }
-        return dp[i] = take;
-    }
-
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 0);
-        // return f(n-1,nums,dp);
-        dp[1] = (nums[0] == nums[1]);
-        if(n==2) return dp[1];
-        dp[2] = (nums[0] == nums[1] and nums[1]==nums[2])|(nums[0]+1 == nums[1] and nums[1]+1==nums[2]);
+        dp[1] = !(nums[0] ^ nums[1]);
+        if (n == 2)
+            return dp[1];
+        dp[2] = (!(nums[0] ^ nums[1]) & !(nums[1] ^ nums[2])) |
+                (!((nums[0] + 1) ^ nums[1]) & !((nums[1] + 1) ^ nums[2]));
         for (int i = 3; i < n; i++) {
-            if (nums[i - 1] == nums[i]) {
-                dp[i] = 1 & dp[i - 2];
-            }
-            if (nums[i - 2] == nums[i - 1] and nums[i - 1] == nums[i]) {
-                dp[i] |= (1 & dp[i - 3]);
-            }
-            if (nums[i - 2] == nums[i - 1] - 1 and nums[i - 1] == nums[i] - 1) {
-                dp[i] |= (1 & dp[i - 3]);
-            }
+            dp[i] = (!(nums[i - 1] ^ nums[i]) & dp[i - 2]) |
+                    ((!(nums[i - 2] ^ nums[i - 1]) & !(nums[i - 1] ^ nums[i]) &
+                      dp[i - 3])) |
+                    ((!(nums[i - 2] ^ (nums[i - 1] - 1)) &
+                      !(nums[i - 1] ^ (nums[i] - 1))) &
+                     dp[i - 3]);
         }
-        return dp[n-1];
+        return dp[n - 1];
     }
 };
