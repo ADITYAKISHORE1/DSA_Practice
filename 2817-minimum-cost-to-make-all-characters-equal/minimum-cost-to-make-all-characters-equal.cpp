b24c8fdc9906c7503fb12cb0f287c658f3e09c23
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-08 17:27:47 +0530

    Time: 4 ms (61.58%) | Memory: 16.9 MB (89.27%) - LeetSync

--- 2817-minimum-cost-to-make-all-characters-equal/minimum-cost-to-make-all-characters-equal.cpp
+++ 2817-minimum-cost-to-make-all-characters-equal/minimum-cost-to-make-all-characters-equal.cpp
@@ -2,23 +2,10 @@ class Solution {
 public:
     long long minimumCost(string s) {
         int n = s.size();
-        if (n == 1)
-            return 0;
-        vector<long long> pref(n);
-        long long ans = LLONG_MAX;
-        pref[0] = 0;
+        long long ans = 0;
         for (int i = 1; i < n; i++) {
-            pref[i] = pref[i - 1];
-            if (s[i] != s[i - 1]) {
-                pref[i] += i;
-            }
-        }
-        long long suf=0;
-        for (int i = n - 2; i >= 0; i--) {
-            if (s[i] != s[i + 1]) {
-                suf += n - i - 1;
-            }
-            ans = min(ans, suf + pref[i]);
+            if (s[i] != s[i - 1])
+                ans += min(i, n - i);
         }
         return ans;
     }
