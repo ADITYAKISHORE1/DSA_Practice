583953ce1b39437e00d068ce775aec6148fe1759
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-09 16:48:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (79.82%) - LeetSync

--- 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -3,18 +3,20 @@ public:
     int minOperations(int n) {
         int ans = 0;
         while (n) {
-            if ((n & 1) == 0) {
+            if ((n & 1) == 0) { // if 0th bit is unset do right shift until you get set bit
                 n >>= 1;
-            } else {
-                if (n == 1) {
-                    ans++;
+            } else { // 0th bit is set
+                ans++;
+                if (n == 1) // if only 0th bit is set bit (Base condition) -> return
                     break;
-                } else {
-                    if ((n & 3) == 3)
+                else { // more than one bit is set
+                    if ((n & 3) == 3) // if continuous set bits from 0th bit are there, so
+                                      // best way will be to add 1 to reduce k consecutive
+                                      // set to 1 set bit bcz 001111 + 1 = 010000
                         n++;
-                    else
+                    else // if no continuous set bit starts from 0th idx
+                         //101001 - 1 = 101000
                         n--;
-                    ans++;
                 }
             }
         }
