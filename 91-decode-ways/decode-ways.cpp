ac11e7f34dbc0fcdeea1946693e9c34017ebb704
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-09 18:04:41 +0530

    Time: 3 ms (16.40%) | Memory: 9 MB (32.37%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,27 +1,20 @@
 class Solution {
-    vector<int> dp;
-    int f(int n, string& s) {
-        if (n < 0)
-            return 1;
-        if (dp[n] != -1)
-            return dp[n];
-        int one = 0;
-        if (s[n] != '0') {
-            one = f(n - 1, s);
-        }
-        if (n > 0) {
-            int val = stoi(s.substr(n - 1, 2));
-            if (val >= 10 and val <= 26) {
-                one += f(n - 2, s);
-            }
-        }
-        return dp[n] = one;
-    }
-
 public:
     int numDecodings(string s) {
         int n = s.size();
-        dp.resize(n, -1);
-        return f(n - 1, s);
+        vector<int> dp(n + 2, 0);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i < n + 2; i++) {
+            if (s[i - 2] != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if (i > 2) {
+                int val = stoi(s.substr(i - 3, 2));
+                if (val >= 10 and val <= 26) {
+                    dp[i] += dp[i - 2];
+                }
+            }
+        }
+        return dp[n + 1];
     }
 };
