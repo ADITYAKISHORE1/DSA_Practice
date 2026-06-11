c3309ab2413c954f39e8c6e32cdb9dd3633a97af
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-11 11:48:22 +0530

    Time: 297 ms (50.34%) | Memory: 335.9 MB (49.67%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    const int MOD = 1e9 + 7;
+    long long expo(long long a,long long b){
+        long long res=1;
+        while(b){
+            if(b&1){
+                res=res*a%MOD;
+            }
+            a=a*a%MOD;
+            b>>=1;
+        }
+        return res;
+    }
+public:
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int n = 0;
+        for (auto& i : edges)
+            n = max({n, i[0], i[1]});
+        vector<vector<int>> adj(n + 1);
+        for (auto& i : edges) {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        // cout<<n<<endl;
+        queue<int> q;
+        vector<long long> dist(n + 1, -1);
+        q.push(1);
+        dist[1] = 0;
+        long long maxmDist = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            maxmDist = max(maxmDist, dist[node]);
+            for (auto& adjNode : adj[node]) {
+                if (dist[adjNode] ==-1) {
+                    dist[adjNode] = dist[node] + 1;
+                    q.push(adjNode);
+                }
+            }
+        }
+        cout << maxmDist;
+        long long ans = expo(2, maxmDist - 1);
+        return ans%MOD;
+    }
+};
