29f89c0feb3f59a05cf50c40092d8fab80eacd6e
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-11 16:34:43 +0530

    Time: 23 ms (63.27%) | Memory: 9.4 MB (91.25%) - LeetSync

--- 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/k-inverse-pairs-array.cpp
@@ -1,34 +1,23 @@
 class Solution {
-    const int MOD=1e9+7;
-    
-    // long long f(int n,int k){
-    //     if(n==0) return 0;
-    //     if(k==0) return 1;
-    //     if(dp[n][k]!=-1) return dp[n][k];
-    //     long long take=0;
-    //     for(int i=0;i<=min(k,n-1);i++){
-            
-    //         take=(take+f(n-1,k-i))%MOD;
-    //     }
-    //     return dp[n][k]=take;
-    // }
+    const int MOD = 1e9 + 7;
+
 public:
     int kInversePairs(int n, int k) {
-        // vector<vector<long long>> dp(n+1,vector<long long>(k+1,0));
-        vector<long long> cur(k+1,0),prev(k+1,0);
-        // for(int i=1;i<=n;i++) dp[i][0]=1;
-        prev[0]=1;
-        for(int i=1;i<=n;i++){
-            fill(cur.begin(),cur.end(),0);
-            cur[0]=1;
-            for(int j=1;j<=k;j++){
-                for(int l=0;l<=min(j,i-1);l++){
-                    cur[j]=(cur[j]+prev[j-l])%MOD;
+        vector<long long> cur(k + 1, 0), prev(k + 1, 0);
+        prev[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            cur[0] = 1;
+            for (int j = 1; j <= k; j++) {
+                cur[j] = prev[j - min(j, i - 1)];
+                if (j + 1 <= k) {
+                    cur[j] = (cur[j] - prev[j + 1] + MOD) % MOD;
                 }
             }
-            prev=cur;
+            prev = cur;
+            for (int l = k - 1; l >= 0; l--) {
+                prev[l] = (prev[l] + prev[l + 1]) % MOD;
+            }
         }
-        // return dp[n][k]%MOD;
         return prev[k];
     }
 };
