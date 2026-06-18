f2d6dabca71463bba388ab3fb402b4dc2639cb59
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-19 04:33:15 +0530

    Time: 131 ms (9.57%) | Memory: 26.2 MB (31.16%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.cpp
+++ 971-shortest-bridge/shortest-bridge.cpp
@@ -7,15 +7,22 @@ class Solution {
     bool isInsideGrid(int& nx, int& ny) {
         return (nx >= 0 and ny >= 0 and nx < n and ny < n);
     }
-    void dfs(int x, int y, vector<vector<int>>& grid) {
+    void bfs(int& x, int& y, vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        q.push({x, y});
         vis[x][y] = 1;
-        island1.push_back({x, y});
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (isInsideGrid(nx, ny) and grid[nx][ny] == 1 and
-                vis[nx][ny] == 0) {
-                dfs(nx, ny, grid);
+        while (!q.empty()) {
+            auto [a, b] = q.front();
+            q.pop();
+            island1.push_back({a,b});
+            for (int i = 0; i < 4; i++) {
+                int nx = a + dx[i];
+                int ny = b + dy[i];
+                if (isInsideGrid(nx, ny) && grid[nx][ny] == 1 &&
+                    vis[nx][ny] == 0) {
+                    vis[nx][ny] = 1;
+                    q.push({nx, ny});
+                }
             }
         }
     }
@@ -29,7 +36,7 @@ public:
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
                     if (!ck) {
-                        dfs(i, j, grid);
+                        bfs(i, j, grid);
                         ck = 1;
                     } else if (vis[i][j] == 0) {
                         island2.push_back({i, j});
@@ -37,6 +44,7 @@ public:
                 }
             }
         }
+
         int ans = INT_MAX;
         for (auto& [x1, y1] : island1) {
             for (auto& [x2, y2] : island2) {
