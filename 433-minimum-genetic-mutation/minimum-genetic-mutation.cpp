3b554996390e0d47c754d3da43ec7df13e1d5aba
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-19 01:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (73.97%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.cpp
@@ -1,35 +1,36 @@
 class Solution {
-    int strDiff(string& s,string& t){
-        int cnt=0;
-        for(int i=0;i<8;i++){
-            cnt+=(s[i]!=t[i]);
+    bool strDiff(string& s, string& t) {
+        int cnt = 0;
+        for (int i = 0; i < 8; i++) {
+            cnt += (s[i] != t[i]);
         }
-        return cnt;
+        return (cnt == 1);
     }
+
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        int n=bank.size();
-        vector<int> dist(n+1,1e9);
-        queue<pair<int,string>> q;
-        q.push({0,startGene});
-        dist[0]=0;
-        while(!q.empty()){
-            auto& [it,tm]=q.front();
+        bank.insert(bank.begin(), startGene);
+        int n = bank.size();
+        vector<int> dist(n, 1e9);
+        queue<int> q;
+        q.push(0);
+        dist[0] = 0;
+        while (!q.empty()) {
+            int& it = q.front();
             q.pop();
-            for(int i=0;i<n;i++){
-                if(strDiff(tm,bank[i])==1 and dist[i+1]>dist[it]+1){
-                    dist[i+1]=dist[it]+1;
-                    q.push({i+1,bank[i]});
+            for (int i = 0; i < n; i++) {
+                if (it != i && strDiff(bank[it], bank[i]) &&
+                    dist[i] > dist[it] + 1) {
+                    dist[i] = dist[it] + 1;
+                    q.push(i);
                 }
             }
         }
-        int ans=-1;
-        for(int i=0;i<n;i++){
-            if(bank[i]==endGene){
-                ans=dist[i+1];
-                break;
+        for (int i = 0; i < n; i++) {
+            if (bank[i] == endGene) {
+                return (dist[i] == 1e9) ? -1 : dist[i];
             }
         }
-        return (ans==1e9)?-1:ans;
+        return -1;
     }
 };
