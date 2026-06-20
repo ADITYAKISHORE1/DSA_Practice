02232d19c71599ef6f67508d0d2a9a8eb7513446
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-21 02:10:28 +0530

    Time: 12 ms (37.50%) | Memory: 9.6 MB (75.00%) - LeetSync

--- 4310-count-good-integers-in-a-range/count-good-integers-in-a-range.cpp
+++ 4310-count-good-integers-in-a-range/count-good-integers-in-a-range.cpp
@@ -1,50 +1,40 @@
 class Solution {
-    long long dp[20][2][2][10];
-    /* dp states
-        i -> digit
-        tight -> check the upper limit
-        started -> to don't count leading zeros
-        digit -> prev digit used to satisfy the difference condition
-    */
-    long long f(int i, bool tight, bool started, int digit, vector<int>& v,
+    long long dp[16][2][2][10];
+    long long f(int i, bool tight, bool started, int prevDigit, vector<int>& v,
                 int k) {
-        if (i == v.size())
-            return 1;
-        if(dp[i][tight][started][digit]!=-1) return dp[i][tight][started][digit];
+        if(i==v.size()) return 1;
+        if(dp[i][tight][started][prevDigit]!=-1) return dp[i][tight][started][prevDigit];
         long long take = 0;
         int lim = (tight) ? v[i] : 9;
         for (int d = 0; d <= lim; d++) {
-            int ntight = (tight && (d == v[i]));
-            if (!started and d == 0) {
-                take += f(i + 1, ntight, 0, d, v, k);
-            } else if (!started) {
-                take += f(i + 1, ntight, 1, d, v, k);
-            } else if (abs(d - digit) <= k) {
+            int ntight = (tight && d == v[i]);
+            if (!started) {
+                take += f(i + 1, ntight, (d != 0), d, v, k);
+            } else if (abs(d - prevDigit) <= k) {
                 take += f(i + 1, ntight, 1, d, v, k);
             }
         }
-        return dp[i][tight][started][digit]=take;
+        return dp[i][tight][started][prevDigit]=take;
     }
 
 public:
     long long goodIntegers(long long l, long long r, int k) {
-        vector<int> vl;
+        vector<int> vl, vr;
         l--;
-        while (l != 0) {
+        while (l) {
             vl.push_back(l % 10);
             l /= 10;
         }
-        vector<int> vr;
-        while (r != 0) {
+        while (r) {
             vr.push_back(r % 10);
             r /= 10;
         }
-        reverse(vl.begin(), vl.end());
-        reverse(vr.begin(), vr.end());
+        reverse(vl.begin(),vl.end());
+        reverse(vr.begin(),vr.end());
         memset(dp,-1,sizeof(dp));
-        long long a = f(0, 1, 0, 0, vl, k);
+        long long ans = f(0, 1, 0, 0, vr, k);
         memset(dp,-1,sizeof(dp));
-        long long b = f(0, 1, 0, 0, vr, k);
-        return b - a;
+        ans-=f(0, 1, 0, 0, vl, k);
+        return ans;
     }
 };
