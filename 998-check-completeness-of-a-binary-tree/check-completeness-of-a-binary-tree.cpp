3be876ec21255fa734da699347ca6a04a8e3a9e5
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-25 06:49:15 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (30.95%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -13,32 +13,16 @@
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        vector<int> v;
         queue<TreeNode*> q;
-        if (root != nullptr) {
-            v.push_back(root->val);
-            q.push(root);
+        q.push(root);
+        while (q.front() != nullptr) {
+            auto r = q.front();
+            q.pop();
+            q.push(r->left);
+            q.push(r->right);
         }
-        while (!q.empty()) {
-            int sz = q.size();
-            if (sz--) {
-                auto r = q.front();
-                q.pop();
-                if (r->left != nullptr) {
-                    v.push_back(r->left->val);
-                    q.push(r->left);
-                } else
-                    v.push_back(-1);
-                if (r->right != nullptr) {
-                    v.push_back(r->right->val);
-                    q.push(r->right);
-                } else
-                    v.push_back(-1);
-            }
-        }
-        for(int i=1;i<v.size()-1;i++){
-            if(v[i]==-1 and v[i+1]>-1) return false; 
-        }
-        return true;
+        while (q.front() == nullptr and !q.empty())
+            q.pop();
+        return q.empty();
     }
 };
