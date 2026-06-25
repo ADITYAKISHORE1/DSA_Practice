5ac6ac023814e5571357efe84d74a979cc8a179b
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-06-25 05:38:48 +0530

    Time: 3 ms (97.29%) | Memory: 25 MB (73.22%) - LeetSync

--- 508-most-frequent-subtree-sum/most-frequent-subtree-sum.cpp
+++ 508-most-frequent-subtree-sum/most-frequent-subtree-sum.cpp
@@ -6,31 +6,33 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-    unordered_map<long long,long long> hash;
-    long long dfs(TreeNode* root){
-        if(root==nullptr) return 0;
-        long long sum=root->val;
-        long long l=dfs(root->left);
-        long long r=dfs(root->right);
-        sum+=l+r;
+    unordered_map<int, int> hash;
+    int dfs(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int sum = root->val + dfs(root->left) + dfs(root->right);
         hash[sum]++;
         return sum;
     }
+
 public:
     vector<int> findFrequentTreeSum(TreeNode* root) {
-        if(root==nullptr) return {};
+        if (root == nullptr)
+            return {};
         vector<int> ans;
         dfs(root);
-        long long maxFreq=0;
-        for(auto& i:hash){
-            maxFreq=max(maxFreq,i.second);
+        int maxFreq = 0;
+        for (auto& i : hash) {
+            maxFreq = max(maxFreq, i.second);
         }
-        for(auto& i:hash){
-            if(i.second==maxFreq) ans.push_back(i.first);
+        for (auto& i : hash) {
+            if (i.second == maxFreq)
+                ans.push_back(i.first);
         }
         return ans;
     }
