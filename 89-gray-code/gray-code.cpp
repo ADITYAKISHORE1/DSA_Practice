7884ea4538e26facb307fb89d633d7abe9b23908
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-07-04 00:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (24.00%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -1,32 +1,13 @@
 class Solution {
-    int maxm;
-    vector<int> ans, v, vis;
-    void dfs(int bit) {
-        if (!ans.empty())
-            return;
-        if (v.size() == maxm) {
-            ans = v;
-            return;
-        }
-        for (int i = 0; i < bit; i++) {
-            int t = v.back() ^ (1 << i);
-            if (t < maxm and vis[t] == 0) {
-                vis[t] = 1;
-                v.push_back(t);
-                dfs(bit);
-                v.pop_back();
-                vis[t] = 0;
-            }
-        }
-    }
-
 public:
     vector<int> grayCode(int n) {
-        maxm = (1 << n);
-        vis.resize(maxm, 0);
-        v.push_back(0);
-        vis[0] = 1;
-        dfs(n);
-        return ans;
+        vector<int> seq(1,0);
+        for(int i=0;i<n;i++){
+            int add=(1<<i);
+            for(int j=seq.size()-1;j>=0;j--){
+                seq.push_back(add+seq[j]);
+            }
+        }
+        return seq;
     }
 };
