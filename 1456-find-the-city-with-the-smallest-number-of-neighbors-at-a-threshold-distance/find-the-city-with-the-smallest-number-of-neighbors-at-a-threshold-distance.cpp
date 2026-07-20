7f101fd3a5e855f21fa5f7a5a11184d8e35db750
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-07-20 12:30:25 +0530

    Time: 9 ms (89.85%) | Memory: 16.5 MB (96.59%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -10,8 +10,9 @@ public:
             dist[i][i] = 0;
         for (int k = 0; k < n; k++) {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < n; j++) {
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
             }
         }
         int smallest = 1e9;
