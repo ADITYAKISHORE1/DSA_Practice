de9ea40449f7803d18f1b1e66c7cfb7b9c2333e7
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-07-25 20:40:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (16.57%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,37 +1,11 @@
-class Solution {    
-    vector<int> computeZ(string& s){
-        int n=s.size();
-        vector<int> z(n,0);
-        int l=0,r=0;
-        for(int k=1;k<n;k++){
-            if(k>r){
-                l=k;
-                r=k;
-                while(r<n and s[r]==s[r-l]) r++;
-                z[k]=r-l;
-                r--;
-            }else{
-                if(z[k-l]+k<r+1){
-                    z[k]=z[k-l];
-                }else{
-                    l=k;
-                    while(r<n and s[r]==s[r-l]) r++;
-                    z[k]=r-l;
-                    r--;
-                }
-            }
-        }
-        return z;
-    }
+class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n=needle.size();
-        string s=needle+"$"+haystack;
-        vector<int> z=computeZ(s);
-        for(int i=0;i<z.size();i++){
-            if(z[i]==n){
-                return i-n-1;
-            }
+        int n=haystack.size();
+        int m=needle.size();
+        for(int i=0;i<=n-m;i++){
+            string s=haystack.substr(i,m);
+            if(s==needle) return i;
         }
         return -1;
     }
