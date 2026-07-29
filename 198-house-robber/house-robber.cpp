62087462f1656c4d96c145f6463718caa9f9992c
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-07-30 01:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.03%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,15 @@
 class Solution {
-    vector<vector<int>> dp;
-    int f(int i,bool t,vector<int>&v){
-        if(i<0) return 0;
-        if(dp[i][t]!=-1) return dp[i][t];
-        int take=0;
-        if(t){
-            take=v[i]+f(i-1,false,v);
-        } 
-        int notTake=f(i-1,true,v);
-        return dp[i][t]=max(take,notTake);
-    }
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        dp.resize(n,vector<int>(2,-1));
-        return f(n-1,true,nums);
+        vector<int> cur(2, 0);
+        vector<int> prev(2, 0);
+        prev[1] = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            cur[1] = nums[i] + prev[0];
+            cur[0] = max(prev[0], prev[1]);
+            prev = cur;
+        }
+
+        return max(prev[true], prev[false]);
     }
 };
