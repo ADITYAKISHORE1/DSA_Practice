5daae4aaff505f33c897deec92208f6f78bfd333
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-07-30 12:48:16 +0530

    Time: 15 ms (28.15%) | Memory: 17.4 MB (21.72%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -3,16 +3,16 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> st(wordDict.begin(),wordDict.end());
         int n=s.size();
-        vector<bool> dp(n,false);
-        // dp[0]=true;
-        for(int i=0;i<n;i++){
+        vector<bool> dp(n+1,false);
+        dp[0]=true;
+        for(int i=1;i<=n;i++){
             bool t=false;
-            for(int j=0;j<=i;j++){
-                string tmp=s.substr(j,i-j+1);
-                if(st.count(tmp)) t|=(j-1<0)?true:dp[j-1];
+            for(int j=1;j<=i;j++){
+                string tmp=s.substr(j-1,i-j+1);
+                if(st.count(tmp)) t|=dp[j-1];
             }
             dp[i]=t;
         }
-        return dp[n-1];
+        return dp[n];
     }
 };
