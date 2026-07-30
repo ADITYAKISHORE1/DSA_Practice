a81ba14cfc1f777b479d90190d70cd3d94cd0f9d
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-07-30 15:31:34 +0530

    Time: 36 ms (51.46%) | Memory: 18.9 MB (60.64%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,19 +1,26 @@
 class Solution {
-    vector<vector<int>> dp;
-    int f(int n,int amt,vector<int>& coins){
-        if(amt==0) return 0;
-        if(n<0) return 1e9;
-        if(dp[n][amt]!=-1) return dp[n][amt];
-        int take=INT_MAX;
-        if(coins[n]<=amt) take=1+f(n,amt-coins[n],coins);
-        int notTake=f(n-1,amt,coins);
-        return dp[n][amt]=min(take,notTake);
+    vector<int> dp;
+    int f(int amt, vector<int>& coins) {
+        if (amt == 0)
+            return 0;
+
+        if (dp[amt] != -1)
+            return dp[amt];
+        dp[amt] = 1e9;
+        for (auto& c : coins) {
+            int take = INT_MAX;
+            if (c <= amt)
+                take = 1 + f(amt - c, coins);
+            dp[amt] = min(take, dp[amt]);
+        }
+        return dp[amt];
     }
+
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        dp.resize(n,vector<int>(amount+1,-1));
-        int ans=f(n-1,amount,coins);
-        return ans>=1e9?-1:ans;
+        int n = coins.size();
+        dp.resize(amount + 1, -1);
+        int ans = f(amount, coins);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
