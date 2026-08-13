6f5305c14818b1ec10c26914ae0d88757121d844
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-08-14 01:16:58 +0530

    Time: 0 ms (100.00%) | Memory: 140 MB (50.24%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n + 1, 0);
+        int a = 0, b = 0, c = 0;
         for (int i = n - 1; i >= 0; i--) {
             int t = INT_MIN;
             int s = stoneValue[i];
-            t = max(t, s - dp[i + 1]);
+            t = max(t, s - a);
             if (i < n - 1) {
                 s += stoneValue[i + 1];
-                t = max(t, s - dp[i + 2]);
+                t = max(t, s - b);
             }
             if (i < n - 2) {
                 s += stoneValue[i + 2];
-                t = max(t, s - dp[i + 3]);
+                t = max(t, s - c);
             }
-            dp[i] = t;
+            c = b;
+            b = a;
+            a = t;
         }
-        if (dp[0] > 0)
+        if (a > 0)
             return "Alice";
-        else if (dp[0] < 0)
+        else if (a < 0)
             return "Bob";
         return "Tie";
     }
