c5082c79e0e7243fe252d171d846de1b818be777
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-08-14 01:14:16 +0530

    Time: 11 ms (84.28%) | Memory: 146.4 MB (13.98%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,22 +1,26 @@
 class Solution {
-    vector<int> dp;
-    int f(int i,int & n,vector<int>& v){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int t=INT_MIN;
-        t=max(t,v[i]-f(i+1,n,v));
-        if(i<n-1) t=max(t,v[i]+v[i+1]-f(i+2,n,v));
-         if (i<n-2) t=max(t,v[i]+v[i+1]+v[i+2]-f(i+3,n,v));
-        return dp[i]=t;
-    }
 public:
     string stoneGameIII(vector<int>& stoneValue) {
-        int n=stoneValue.size();
-        dp.resize(n,-1);
-        int v=f(0,n,stoneValue);
-        // cout<<v;
-        if(v>0) return "Alice";
-        else if(v<0) return "Bob";
+        int n = stoneValue.size();
+        vector<int> dp(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            int t = INT_MIN;
+            int s = stoneValue[i];
+            t = max(t, s - dp[i + 1]);
+            if (i < n - 1) {
+                s += stoneValue[i + 1];
+                t = max(t, s - dp[i + 2]);
+            }
+            if (i < n - 2) {
+                s += stoneValue[i + 2];
+                t = max(t, s - dp[i + 3]);
+            }
+            dp[i] = t;
+        }
+        if (dp[0] > 0)
+            return "Alice";
+        else if (dp[0] < 0)
+            return "Bob";
         return "Tie";
     }
 };
