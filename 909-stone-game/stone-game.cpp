41dfe63b4334eb3b56351bcb6cef2f7065a6f1e8
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-08-13 15:01:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (66.18%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,16 +1,6 @@
 class Solution {
-    vector<vector<int>> dp;
-    int f(int l,int r,vector<int>&v){
-        if(l>r) return 0;
-        if(dp[l][r]!=-1) return dp[l][r];
-        int takel=v[l]-f(l+1,r,v);
-        int taker=v[r]-f(l,r-1,v);
-        return dp[l][r]=max(takel,taker);
-    }
 public:
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        dp.resize(n,vector<int>(n,-1));
-        return f(0,n-1,piles);
+        return true;
     }
 };
