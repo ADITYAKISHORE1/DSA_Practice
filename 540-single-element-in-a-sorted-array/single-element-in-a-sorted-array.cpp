ca59ad08159f4d408dbff5c0aa4601daf4d4d967
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-08-29 15:39:32 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (70.24%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        int l=0,r=n-1;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if((mid>0 and nums[mid]==nums[mid-1]) or (mid<n-1 and nums[mid]==nums[mid+1])){
-                if(mid>0 and nums[mid]==nums[mid-1]){
-                    int lsize=mid+1;
-                    int rsize=n-mid-1;
-                    if(lsize%2==0){
-                        l=mid+1;
-                    }else r=mid-1;
-                }else{
-                    int lsize=mid;
-                    int rsize=n-mid;
-                    if(rsize%2==0){
-                        r=mid-1;
-                    }else l=mid+1;
-                }
-            }else return nums[mid];
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (mid > 0 and nums[mid] == nums[mid - 1]) {
+                if ((mid + 1) % 2 == 0) {
+                    l = mid + 1;
+                } else
+                    r = mid - 1;
+            } else if (mid < n - 1 and nums[mid] == nums[mid + 1]) {
+                if ((n - mid) % 2 == 0) {
+                    r = mid - 1;
+                } else
+                    l = mid + 1;
+            } else
+                return nums[mid];
         }
         return -1;
     }
