94b536d54a5802229d0ab10111b4f69e05b6652d
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-08-31 12:36:41 +0530

    Time: 8 ms (55.23%) | Memory: 11.3 MB (94.52%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size(), m = t.size();
-        unordered_map<char, int> mpp;
-        for (char& c : t) {
-            mpp[c]++;
-        }
-        int head = 0, win = n;
-        int start = 0, cnt = m;
-        for (int end = 0; end < n; end++) {
-            if (mpp[s[end]] > 0) {
+        int m=s.size(),n=t.size();
+        unordered_map<char,int> mpp;
+        for(auto& c:t) mpp[c]++;
+        int head=0,win=m;
+        int cnt=n;
+        int start=0,end=0;
+        while(end<m){
+            if(mpp[s[end]]>0){
                 cnt--;
             }
             mpp[s[end]]--;
+
             while(cnt==0){
-                while(start<=end){
-                    if(!mpp.count(s[start])) start++;
-                    else if(mpp[s[start]]<0){
+                while(1){
+                    if(!mpp.count(s[start])){
+                        start++;
+                    }else if(mpp[s[start]]<0){
                         mpp[s[start]]++;
                         start++;
                     }else break;
                 }
                 if(end-start<win){
-                    head=start;
                     win=end-start;
+                    head=start;
                 }
                 cnt++;
                 mpp[s[start]]++;
                 start++;
             }
-            
+            end++;
         }
-        if (win == n)
-            return "";
-        return s.substr(head, win + 1);
+        if(win==m) return "";
+        return s.substr(head,win+1);
     }
 };
