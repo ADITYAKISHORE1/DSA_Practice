b6c86f342409dd376c4b660836f75e01d929291a
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-09-05 13:06:43 +0530

    Time: 8 ms (93.92%) | Memory: 79.7 MB (99.07%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,20 +2,71 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        int l=0;
+
+        if (n <= 1)
+            return n;
+
+        int l = 0, r = 0;
         int maxFruit = 0;
-        unordered_map<int,int> freq;
-        for(int r=0;r<n;r++){
-            freq[fruits[r]]++;
-            while(l<n and freq.size()>2){
-                freq[fruits[l]]--;
-                if(freq[fruits[l]]==0){
-                    freq.erase(fruits[l]);
-                }
-                l++;
+
+        int f1 = -1, f2 = -1;
+        int last1 = -1, last2 = -1;
+
+        while (r < n) {
+
+            if (f1 == -1) {
+                f1 = fruits[r];
+                last1 = r;
+                r++;
             }
-            maxFruit=max(maxFruit,r-l+1);
+
+            else if (f2 == -1) {
+                if (fruits[r] == f1) {
+                    last1 = r;
+                } else {
+                    f2 = fruits[r];
+                    last2 = r;
+                }
+                r++;
+            }
+
+            else if (fruits[r] == f1) {
+                last1 = r;
+                r++;
+            }
+
+            else if (fruits[r] == f2) {
+                last2 = r;
+                r++;
+            }
+
+            else {
+                maxFruit = max(maxFruit, r - l);
+                if (last1 < last2) {
+                    l = last1 + 1;
+
+                    f1 = f2;
+                    last1 = last2;
+
+                    f2 = fruits[r];
+                    last2 = r;
+                }
+                else {
+                    l = last2 + 1;
+
+                    f2 = f1;
+                    last2 = last1;
+
+                    f1 = fruits[r];
+                    last1 = r;
+                }
+
+                r++;
+            }
         }
+
+        maxFruit = max(maxFruit, r - l);
+
         return maxFruit;
     }
 };
