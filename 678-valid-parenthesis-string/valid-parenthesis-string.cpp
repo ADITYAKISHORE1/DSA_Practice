09a7533d757ed9cc8eabc0a4e890a81c9b699bc7
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-09-05 23:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (27.84%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,26 +1,28 @@
-class Solution {
-    vector<vector<int>> dp;
-    int n;
-    bool f(int i,int cnt,string& s){
-        if(cnt<0) return false;
-        if(i==s.size()){
-            if(cnt==0) return true;
-            return false;
-        }
-        if(dp[i][cnt]!=-1) return dp[i][cnt];
-        if(s[i]=='('){
-            return dp[i][cnt]=f(i+1,cnt+1,s);
-        }
-        else if(s[i]==')'){
-            return dp[i][cnt]=f(i+1,cnt-1,s);
-        }
-        return dp[i][cnt]=f(i+1,cnt+1,s)|f(i+1,cnt-1,s)|f(i+1,cnt,s);
-    }
-public:
-    bool checkValidString(string s) {
-        n=s.size();
-        dp.resize(n,vector<int>(n,-1));
-        return f(0,0,s);
-    }
+class Solution { 
+public: 
+    bool checkValidString(string s) { 
+        stack<int> open, star; 
+ 
+        for(int i = 0; i < size(s); i++) { 
+            if(s[i] == '(') 
+                open.push(i); 
+            else if(s[i] == '*') 
+                star.push(i); 
+            else { 
+                if(!open.empty()) 
+                    open.pop(); 
+                else if(!star.empty()) 
+                    star.pop(); 
+                else 
+                    return false; 
+            } 
+        } 
+ 
+        while(!open.empty() && !star.empty() && star.top() > open.top()) { 
+            open.pop(); 
+            star.pop(); 
+        } 
+ 
+        return open.empty(); 
+    } 
 };
-// ((*))())
