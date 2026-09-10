4b062c608ebe10085d96ab28382b2478e30edb70
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-09-11 01:04:25 +0530

    Time: 12 ms (60.80%) | Memory: 30.1 MB (53.06%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -5,7 +5,7 @@ public:
         if(n%2==1) return false;
         int balance=0;
         for(int i=0;i<n;i++){ // checking for "can we avoid excess ) "
-            if(s[i]=='(' or locked[i]=='0'){
+            if(s[i]=='(' || locked[i]=='0'){
                 balance++;
             }else balance--;
             if(balance<0) return false;
@@ -13,7 +13,7 @@ public:
         // at last balance will be always even bcz n will be even and add or substract of two even or two odd is always even.
         balance=0;
         for(int i=n-1;i>=0;i--){
-            if(s[i]==')' or locked[i]=='0'){ // checking for "can we avoid excess ( "
+            if(s[i]==')' || locked[i]=='0'){ // checking for "can we avoid excess ( "
                 balance++;
             }else balance--;
             if(balance<0) return false;
