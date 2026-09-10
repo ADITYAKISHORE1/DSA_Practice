8e82c4ab30c1ee9913f70959b0a67a12af679982
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-09-11 01:03:37 +0530

    Time: 23 ms (9.42%) | Memory: 30.2 MB (53.06%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n=s.size();
+        if(n%2==1) return false;
+        int balance=0;
+        for(int i=0;i<n;i++){ // checking for "can we avoid excess ) "
+            if(s[i]=='(' or locked[i]=='0'){
+                balance++;
+            }else balance--;
+            if(balance<0) return false;
+        }
+        // at last balance will be always even bcz n will be even and add or substract of two even or two odd is always even.
+        balance=0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==')' or locked[i]=='0'){ // checking for "can we avoid excess ( "
+                balance++;
+            }else balance--;
+            if(balance<0) return false;
+        }
+        return true;
+    }
+};
