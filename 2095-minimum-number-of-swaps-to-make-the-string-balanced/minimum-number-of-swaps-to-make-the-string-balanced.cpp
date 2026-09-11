a92b2ea1323063aef1c710d35123b2994d44a757
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-09-11 15:05:37 +0530

    Time: 6 ms (91.06%) | Memory: 34.2 MB (36.42%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int n = s.size();
-        int open = 0;
+        int imbalance = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (s[i] == ']' && open > 0) {
-                open--;
+        for (char&c:s) {
+            if (c == ']' && imbalance > 0) {
+                imbalance--;
                 continue;
             }
-            open++;
+            imbalance++;
         }
-        cout << open;
-        return open / 2 > 0 ? open / 2 : 0;
+        return imbalance / 2;
     }
 };
