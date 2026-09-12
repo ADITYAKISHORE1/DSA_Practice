06006ff815c16e20b6e8867c63ad9d8f4b045117
ADITYA KISHORE <138054499+ADITYAKISHORE1@users.noreply.github.com>
2026-09-12 13:09:53 +0530

    Time: 18 ms (16.33%) | Memory: 19.1 MB (68.86%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -17,27 +17,18 @@ public:
 */
 
 class Solution {
+    void dfs(Node* root,Node* nxt){
+        if(root==nullptr) return;
+        root->next=nxt;
+        dfs(root->left,root->right);
+        if(nxt!=nullptr){
+            dfs(root->right,nxt->left);
+            dfs(nxt->left,nxt->right);
+        }
+    }
 public:
     Node* connect(Node* root) {
-        if (root == nullptr)
-            return root;
-        queue<Node*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            while (n--) {
-                Node* p = q.front();
-                q.pop();
-                Node* nxt = nullptr;
-                if (!q.empty() and n > 0)
-                    nxt = q.front();
-                p->next = nxt;
-                if (p->left != nullptr)
-                    q.push(p->left);
-                if (p->right != nullptr)
-                    q.push(p->right);
-            }
-        }
+        dfs(root,nullptr);
         return root;
     }
 };
